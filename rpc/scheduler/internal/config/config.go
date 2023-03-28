aab516d8a6d33e6f5a1ed43b99bddb58fd517ddb
CyberOMD <yo.zhouq@gmail.com>
2023-03-28 19:31:33 +0800

    feat(scheduler): add scheduler name, host and port to scheduler.yaml refactor(config.go): change yaml tags to json tags in Task struct fields refactor(config.go): change zrpc.RpcServerConf to service.ServiceConf in Config struct feat(config.go): add Redis field to Config struct

--- rpc/scheduler/internal/config/config.go
+++ rpc/scheduler/internal/config/config.go
@@ -1,16 +1,20 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+)
 
 type Task struct {
 	// 任务名称
-	TaskName string `yaml:"task_name"`
+	TaskName string `json:"task_name"`
 	// cron类型
-	Cronspec string `yaml:"cronspec"`
+	Cronspec string `json:"cronspec"`
 }
 
 type Config struct {
-	zrpc.RpcServerConf
+	service.ServiceConf
+	Redis redis.RedisConf
 	// 任务列表
-	Tasks []Task `yaml:"Tasks"`
+	Tasks []Task
 }
