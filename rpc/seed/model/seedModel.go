a5a9a0ffdf086271001abeb23b5a37f76df9e2e4
CyberOMD <yo.zhouq@gmail.com>
2023-03-29 17:02:31 +0800

    fix(addseedlogic.go): change InsertScreenName to InsertUserName method call in AddSeed function
    refactor(seedModel.go): change `twitter_id`, `name`, `location`, `url`, `description`, `profile_image_url`, and `latest_tweet_id` fields to use `sql.NullString` instead of `sql.NullInt64`
    feat(seedModel.go): add `username` field and change `screen_name` to `username`
    feat(seedModel.go): add `InsertUserName` and `FindOneByUserName` methods to `SeedModel` interface
    refactor(seedModel.go): change `InsertScreenName` to `InsertUserName` and update implementation to use `username` field instead of `screen_name`
    refactor(seedModel.go): change `FindOneByScreenName` to `FindOneByUserName` and update implementation to use `username` field instead of `screen_name`

--- rpc/seed/model/seedModel.go
+++ rpc/seed/model/seedModel.go
@@ -10,14 +10,14 @@ import (
 type (
 	Seed struct {
 		Id              int64          `gorm:"column:id;primaryKey"`
-		TwitterId       sql.NullInt64  `gorm:"column:twitter_id"`
+		TwitterId       sql.NullString `gorm:"column:twitter_id"`
 		Name            sql.NullString `gorm:"column:name"`
-		ScreenName      string         `gorm:"column:screen_name;uniqueIndex;not null"`
+		UserName        string         `gorm:"column:username;uniqueIndex"`
 		Location        sql.NullString `gorm:"column:location"`
 		Url             sql.NullString `gorm:"column:url"`
 		Description     sql.NullString `gorm:"column:description"`
 		ProfileImageUrl sql.NullString `gorm:"column:profile_image_url"`
-		LatestTweetId   sql.NullInt64  `gorm:"column:latest_tweet_id"`
+		LatestTweetId   sql.NullString `gorm:"column:latest_tweet_id"`
 		LatestTweetAt   sql.NullTime   `gorm:"column:latest_tweet_at"`
 		CreateAt        int64          `gorm:"column:create_at;autoCreateTime:milli"`
 		UpdateAt        int64          `gorm:"column:update_at;autoUpdateTime:milli"`
@@ -26,10 +26,10 @@ type (
 
 	SeedModel interface {
 		Insert(ctx context.Context, data *Seed) error
-		InsertScreenName(ctx context.Context, screenName string) error
+		InsertUserName(ctx context.Context, username string) error
 
 		FindOne(ctx context.Context, id int64) (*Seed, error)
-		FindOneByScreenName(ctx context.Context, screenName string) (*Seed, error)
+		FindOneByUserName(ctx context.Context, username string) (*Seed, error)
 		Update(ctx context.Context, data *Seed) error
 	}
 
@@ -55,8 +55,8 @@ func (sr *SeedRepo) Insert(ctx context.Context, seed *Seed) error {
 	return result.Error
 }
 
-func (sr *SeedRepo) InsertScreenName(ctx context.Context, screenName string) error {
-	result := sr.repo.DB(ctx).Create(&Seed{ScreenName: screenName})
+func (sr *SeedRepo) InsertUserName(ctx context.Context, username string) error {
+	result := sr.repo.DB(ctx).Create(&Seed{UserName: username})
 	return result.Error
 }
 
@@ -66,9 +66,9 @@ func (sr *SeedRepo) FindOne(ctx context.Context, id int64) (*Seed, error) {
 	return &seed, result.Error
 }
 
-func (sr *SeedRepo) FindOneByScreenName(ctx context.Context, screenName string) (*Seed, error) {
+func (sr *SeedRepo) FindOneByUserName(ctx context.Context, username string) (*Seed, error) {
 	var seed Seed
-	result := sr.repo.DB(ctx).Where("screen_name = ?", screenName).First(&seed)
+	result := sr.repo.DB(ctx).Where("username = ?", username).First(&seed)
 	return &seed, result.Error
 }
 
