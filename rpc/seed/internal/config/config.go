3d9377babbc62b5417356a01fb827a2d8887f446
CyberOMD <yo.zhouq@gmail.com>
2023-04-03 18:35:03 +0800

    feat(seed): add Twitter API keys to seed.yaml and update Config struct to include TwitterConfig

--- rpc/seed/internal/config/config.go
+++ rpc/seed/internal/config/config.go
@@ -2,7 +2,13 @@ package config
 
 import "github.com/zeromicro/go-zero/zrpc"
 
+type TwitterConfig struct {
+	ApiKey       string `json:"APIKey"`
+	ApiSecretKey string `json:"APISecretKey"`
+}
+
 type Config struct {
 	zrpc.RpcServerConf
 	DataSource string
+	Twitter    TwitterConfig
 }
